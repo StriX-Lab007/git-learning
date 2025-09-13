7dd62cbd457c3511f833ed246b9983b94bc5f02a
StriX-Lab007 <sashivardhan.dev@gmail.com>
2025-09-13 23:45:24 +0530

    first python Code

---
+++ python1.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a + b
+print("The sum is:", sum)
+print("This is a simple addition program.")
+
+sub = a - b
+print("The subtraction is:", sub)
+
+mul = a * b
+print("The multiplication is:", mul)
+div = a / b
+print("The division is:", div)
